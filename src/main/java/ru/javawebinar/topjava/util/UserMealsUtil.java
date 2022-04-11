8ce97a1c724185cdb0cef2b3a2c5ce8f47f86ff2
Frenchfan <sumkin.gpb@gmail.com>
2022-04-12 02:27:33 +0300

    Changed projects's structure to make it work

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -1,7 +1,7 @@
 package ru.javawebinar.topjava.util;
 
 import ru.javawebinar.topjava.model.UserMeal;
-import ru.javawebinar.topjava.model.UserMealWithExcess;
+import topjava.src.main.java.ru.javawebinar.topjava.model.UserMealWithExcess;
 
 import java.time.LocalDateTime;
 import java.time.LocalTime;
